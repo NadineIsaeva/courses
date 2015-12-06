fc86e91d31e3045ab948466e1223682fbe084f32
NadineIsaeva <nadineisabsent@gmail.com>
2015-12-06 23:15:42 +0200

    f
    f

--- courses/prog_base/tasks/dyn_mem/main.c
+++ courses/prog_base/tasks/dyn_mem/main.c
@@ -1,46 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h> // for malloc and free
-#include <ctype.h> // for islower and isalpha
-#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(void)
 {
-    char * inputString = NULL;
-    char * resultString = NULL;
-    inputString = (char *) malloc(50 * sizeof(char));
-    resultString = (char *) malloc(5 * sizeof(char));
-
-    if (NULL == inputString || NULL == resultString)
-    {
-        printf("Error of memory allocation");
-        return EXIT_FAILURE;
-    }
-
+    char * inputString = (char *) calloc(50, sizeof(char));
     printf("Enter your string, please: ");
-    scanf("%s", inputString);
+    gets(inputString);
 
-    while(NULL != *inputString)
-    {
-        if (islower(*inputString) != 0)
+    while (*inputString)
         {
-            *resultString = *inputString;
-            resultString[1] = '\0';
-        }
+            if ((*(inputString + 1)) == '\0')
+            {
+                if (*inputString >= 'a' && *inputString <= 'z')
+                {
+                    printf("%c", *inputString);
+                    break;
+                }
+                else
+                {
+                    putchar('0');
+                    break;
+                }
+            }
         *inputString++;
-    }
+        }
 
-    if (isalpha(resultString[0]) == 0)
-    {
-        putchar('0');
-    }
-    else
-    {
-        printf("The last lower letter is: ");
-        putchar(resultString[0]);
-    }
+ free(inputString);
 
-    free(inputString);
-    free(resultString);
-    return EXIT_SUCCESS;
+return 0;
 }
-
