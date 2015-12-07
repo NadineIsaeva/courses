c5bef8795429621a41a5641b5d08d4b1dc593f68
NadineIsaeva <nadineisabsent@gmail.com>
2015-12-07 02:02:12 +0200

    o
    o

--- courses/prog_base/tasks/dyn_mem/main.c
+++ courses/prog_base/tasks/dyn_mem/main.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h> //for fgets
+#include <string.h>
 #include <ctype.h>
 
 int main(void)
 {
-    char * inputString = (char *) calloc(50, sizeof(char));
+    char * inputString = (char *)malloc(50 * sizeof(char));
+    int * i = (int *)malloc(sizeof(int));
+    char * result = (char *)malloc(sizeof(char));
+    *i = 50;
+    *result = 0;
     printf("Enter your string, please: ");
-    gets(inputString);
-
-    while (*inputString)
+    fgets(inputString, *i, stdin);
+    for(*i = 0; *i < strlen(inputString) - 1; (*i)++)
         {
-            if ((*(inputString + 1)) == '\0')
-            {
-                if (*inputString >= 'a' && *inputString <= 'z')
-                {
-                    printf("%c", *inputString);
-                    break;
-                }
-                else
-                {
-                    putchar('0');
-                    break;
-                }
-            }
-        *inputString++;
+            if(islower(inputString[*i]))
+            *result = inputString[*i];
         }
-
- free(inputString);
-
-return 0;
+    if (*result == 0)
+    {
+       puts("0");
+    }
+     else
+     {
+         puts (result);
+     }
+    free(inputString);
+    free(i);
+    free(result);
+    return EXIT_SUCCESS;
 }
